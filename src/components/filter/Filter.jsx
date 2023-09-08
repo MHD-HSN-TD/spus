a54e87662695bd2556d51220c6b9526dd23a189b
MHD-HSN-TD <8883597!@mhd>
2023-09-08 20:33:50 +0300

    there is a bug here in statemangment

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,22 +1,30 @@
 'use client'
 
-const Filter = () => {
-    const OptionValueHndler = e => console.log(e.target.value)
+import { setFilter } from "@/store/filterSlice"
+import { univFields } from "@/store/uniFields"
+import { useDispatch } from "react-redux"
 
+const Filter = () => {
+
+    const dispatch = useDispatch();
+    const OptionValueHandler = e => {
+        dispatch(setFilter(univFields, "civil"))
+        console.log(e.target.value)
+    }
     return (
         <div className="p-6">
 
             <label htmlFor="category" className=" text-lg font-bold">اختر الفرع المطلوب :  </label>
             <select
                 className="select  select-success w-full max-w-xs px-10 my-4" dir='rtl'
-                id="category" onChange={OptionValueHndler}>
+                id="category" onChange={OptionValueHandler}>
                 <option disabled selected></option>
-                <option value="it" className="text-l pt-10">الهندسة المعلوماتية</option>
-                <option value="civil" className="text-l">الهندسة المدنية</option>
-                <option value="arch" className="text-l">هندسة العمارة</option>
-                <option value="medicen" className="text-l">الطب البشري</option>
+                <option defaultValue="it" value='it' className="text-l pt-10">الهندسة المعلوماتية</option>
+                <option defaultValue='civil' value="civil" className="text-l">الهندسة المدنية</option>
+                <option defaultValue="arch" value='arch' className="text-l">هندسة العمارة</option>
+                <option defaultValue="medicine" value='medicine' className="text-l">الطب البشري</option>
             </select>
-        </div>
+        </div >
     )
 }
 export default Filter
--- src/store/filterSlice.js
+++ src/store/filterSlice.js
@@ -1,14 +1,48 @@
 // "use client";
-import { unv } from "@/store/univs";
 import { createSlice } from "@reduxjs/toolkit";
+import { univFields } from "@/store/uniFields";
 
-const initialUni = { data: unv };
-const filterSlice = createSlice({
+function searchInArray(arr, _, searchElement) {
+  for (let i = 0; i < arr.length; i++) {
+    const obj = arr[i];
+    console.log(searchElement);
+    if (obj.fields.includes(searchElement)) {
+      console.log(obj);
+      return obj;
+    }
+  }
+  return null; // Return null if no matching element is found in any object
+}
+
+const findParentObject = (arr, name) => {
+  for (const obj of arr) {
+    if (obj.fields.some((item) => item.name === name)) {
+      return obj;
+    }
+  }
+
+  return null;
+};
+
+const initialUni = { data: univFields };
+export const filterSlice = createSlice({
   name: "filter",
   initialState: initialUni,
   reducers: {
-    setFilter: (state, payload) => {
-      state.data = state.data.filter((uni) => uni.payload === true);
+    setFilter: (state, action) => {
+      console.log(state.data);
+      console.log(state);
+      // state.data = state.data.find((uni) => {
+      //   console.log(action.payload);
+      //   return (state.data = uni.fields);
+
+      //   console.log(uni.fields.map((field) => field == action.payload));
+      //   return uni.fields.map((field) => field == action.payload);
+      //   // return; //TODO: return the matched results this is the final returns
+      // });
+      // state.data = _.mapValues(state.data, function (field) {
+      //   return field== payload;
+      // });
     },
   },
 });
