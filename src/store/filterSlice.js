a592176fb5ad94559584fcade0461c81cce0ca49
MHD-HSN-TD <8883597!@mhd>
2023-09-12 00:19:03 +0300

    fix to show all data on first time on unis

--- src/store/filterSlice.js
+++ src/store/filterSlice.js
@@ -2,46 +2,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { univFields } from "@/store/uniFields";
 
-// function searchInArray(arr, _, searchElement) {
-//   for (let i = 0; i < arr.length; i++) {
-//     const obj = arr[i];
-//     console.log(searchElement);
-//     if (obj.fields.includes(searchElement)) {
-//       console.log(obj);
-//       return obj;
-//     }
-//   }
-//   return null; // Return null if no matching element is found in any object
-// }
-
-// const findParentObject = (arr, name) => {
-//   for (const obj of arr) {
-//     if (obj.fields.some((item) => item.name === name)) {
-//       return obj;
-//     }
-//   }
-//   return null;
-// };
-
-const initialUni = { chosenField: "it" };
+const initialUni = { chosenField: "" };
 export const filterSlice = createSlice({
   name: "filters",
   initialState: initialUni,
   reducers: {
     setFilter: (state, action) => {
       state.chosenField = action.payload;
-
-      // state.data = state.data.find((uni) => {
-      //   console.log(action.payload);
-      //   return (state.data = uni.fields);
-
-      //   console.log(uni.fields.map((field) => field == action.payload));
-      //   return uni.fields.map((field) => field == action.payload);
-      //   // return; //TODO: return the matched results this is the final returns
-      // });
-      // state.data = _.mapValues(state.data, function (field) {
-      //   return field== payload;
-      // });
     },
   },
 });
