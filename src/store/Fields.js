3ef366a157ee94f97b123857397404e3b166fe21
MHD-HSN-TD <8883597!@mhd>
2023-09-17 17:39:24 +0300

    update the university arr

--- src/store/Fields.js
+++ src/store/Fields.js
@@ -4,6 +4,8 @@
 // Physical Therapy (PT)
 // Health Sciences (HS)
 // Nursing (NRS)
+// biomedical engineering (BA)
+// Hospital Administration (HA)
 // Cosmetology (COS)
 // Computer and Communication Engineering (CCE)
 // Civil Engineering (CE)
@@ -39,96 +41,108 @@ export const fields = [
   },
   {
     Id: 4,
+    Arabic: "الهندسة الطبية",
+    English: "Biomedical Engineering ",
+    Acronym: "BE",
+  },
+  {
+    Id: 5,
+    Arabic: "ادارة المشافي",
+    English: "Hospital Administration",
+    Acronym: "HA",
+  },
+  {
+    Id: 6,
     Arabic: "العلاج الفيزيائي",
     English: "Physical Therapy",
     Acronym: "PT",
   },
   {
-    Id: 5,
+    Id: 7,
     Arabic: "العلوم الصحية",
     English: "Health Sciences",
     Acronym: "HS",
   },
   {
-    Id: 6,
+    Id: 8,
     Arabic: "التمريض",
     English: "Nursing",
     Acronym: "NRS",
   },
   {
-    Id: 7,
+    Id: 9,
     Arabic: "التجميل",
     English: "Cosmetology",
     Acronym: "COS",
   },
   {
-    Id: 8,
+    Id: 10,
     Arabic: "الهندسة المعلوماتية والإتصالات",
     English: "Computer and Communication Engineering",
     Acronym: "CCE",
   },
   {
-    Id: 9,
+    Id: 11,
     Arabic: "الهندسة المدنية",
     English: "Civil Engineering",
     Acronym: "CE",
   },
   {
-    Id: 10,
+    Id: 12,
     Arabic: "الهندسة المعمارية",
     English: "Architectural Engineering",
     Acronym: "AE",
   },
   {
-    Id: 11,
+    Id: 13,
     Arabic: "الهندسة الكيميائية",
     English: "Chemical Engineering",
     Acronym: "CHE",
   },
   {
-    Id: 12,
+    Id: 14,
     Arabic: "الهندسة البترولية",
     English: "Petroleum Engineering",
     Acronym: "PE",
   },
   {
-    Id: 13,
+    Id: 15,
     Arabic: "ادارة الاعمال",
     English: "Business Admin",
     Acronym: "BA",
   },
   {
-    Id: 14,
+    Id: 16,
     Arabic: "الحقوق",
     English: "Law",
     Acronym: "LAW",
   },
   {
-    Id: 15,
+    Id: 17,
     Arabic: "ريادة الأعمال",
     English: "Entrepreneurship",
     Acronym: "ENT",
   },
   {
-    Id: 16,
+    Id: 18,
     Arabic: "اللغة الإنكليزية",
     English: "English Language",
     Acronym: "EL",
   },
   {
-    Id: 17,
+    Id: 19,
     Arabic: "العلاقات الدولية والدبلوماسية",
     English: "International Relations and Diplomacy",
     Acronym: "IRD",
   },
   {
-    Id: 18,
+    Id: 20,
     Arabic: "الفنون",
     English: "Arts",
     Acronym: "ART",
   },
   {
-    Id: 19,
+    Id: 21,
     Arabic: "التمثيل",
     English: "Acting",
     Acronym: "ACT",
