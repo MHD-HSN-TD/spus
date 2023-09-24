6b5871d8b5ae71c98dc9a7b2493a79cd08261d8b
MHD-HSN-TD <8883597!@mhd>
2023-09-25 00:22:31 +0300

    edit style

--- src/app/(dynamic)/unis/[uni_id]/page.jsx
+++ src/app/(dynamic)/unis/[uni_id]/page.jsx
@@ -29,7 +29,16 @@ export async function generateMetadata({ params, searchParams }) {
 
 const Uni = (props) => {
 
+    const date = () => {
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0'); //janvier = 0
+        let yyyy = today.getFullYear();
 
+        return `${yyyy}-${mm}-${dd}`;
+        //return dd + '/' + mm + '/' + yyyy; // change form if you need
+    }
+    // console.log(date())
     const idOfPage = props.params.uni_id
 
     const layoutOfUniv = universities.map(uni => (uni.id == idOfPage) && <div className="  " key={uni.id} >
@@ -113,11 +122,11 @@ const Uni = (props) => {
                     <div className="alert alert-warning mt-6">
                         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
                         <span className="font-medium">المعلومات الخاصة بأسعار الساعات لم تصدر لجميع الكليات حيث يتم تحديث المعلومات يومياً</span>
-                        <p className="font-medium">اخر  تحديث تم بتاريخ : <span>24/9/2023</span></p>
+                        <p className="font-medium">اخر  تحديث تم بتاريخ : <span dir="ltr">{date()}</span></p>
                     </div>
                 </div>
                 {/* table display */}
-                <div className="lg:text-lg  p-4 ">
+                <div className="lg:text-lg  lg:p-4 p-1 ">
                     <div className="text-center">
                         <Table id={idOfPage}></Table>
 
