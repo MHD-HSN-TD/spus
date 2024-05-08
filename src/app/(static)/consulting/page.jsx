3908bd47afcbd44c371053ec7342209391dc6b38
MHD-HSN-TD <8883597!@mhd>
2024-05-08 16:01:34 +0300

    modify the consulting btn style

--- src/app/(static)/consulting/page.jsx
+++ src/app/(static)/consulting/page.jsx
@@ -14,7 +14,7 @@ const about = () => {
     return (
         <section className=" bg">
             {/* -------------Header------------- */}
-            <div className="flex lg:flex-row flex-col-reverse   justify-center items-center lg:px-32 px-10  ">
+            <div className="flex lg:flex-row flex-col-reverse   justify-center items-center lg:p-32 px-10  ">
                 <div>
                     <h1 className="lg:text-2xl text-lg text-warning my-4 font-semibold ">إحصل على<span className=""> إستشارة </span> :</h1>
                     <h3 className="lg:text-2xl text-sm font-medium leading-loose text-justify">{whoISUs}</h3>
@@ -24,14 +24,15 @@ const about = () => {
             {/* -------------Header ends------------- */}
 
 
-            <div className=" lg:px-32 px-10 my-10 ">
+            <div className="  lg:px-32 p-10 my-8  ">
                 <div>
                     <h1 className="lg:text-2xl text-lg text-warning my-4 font-semibold ">تواصل<span className="">   معنـــــــــــــا عبر  </span> :</h1>
                     <div className="lg:text-2xl text-center font-medium leading-loose ">
                         <p>{whoISUs}</p>
 
                         <a href={`https://wa.me/${whatsappNo}`} target='_blank'>
-                            <button className="btn btn-wide my-8 bg-accent hover:bg-success transition-all duration-500"
+                            <button className="btn btn-wide my-8 bg-accent hover:bg-success transition-all duration-700
+                            text-white hover:text-black hover:w-72"
                             >اضغط للإنتقال الى المحادثة</button>
                         </a>
                     </div>
