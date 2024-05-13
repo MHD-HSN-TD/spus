dbc40172dad23f63e43683df6d30fb24940a413c
MHD-HSN-TD <8883597!@mhd>
2024-05-13 17:18:05 +0300

    add the FAQ page and make it better

--- src/app/(static)/faq/page.jsx
+++ src/app/(static)/faq/page.jsx
@@ -5,42 +5,50 @@ export const metadata = {
     title: ' الاستشارات',
     description: 'الموقع الأول في سورية للجامعات السورية الخاصة الذي يقدم استشارات حول التسجيل ',
 }
-const whoISUs = ` نحن فريق مهمتنا تزويدكم بمعلومات شاملة ومحدثة حول جميع الجامعات الخاصة في سوريا. 
-نحن نتفهم أن اختيار الجامعة المناسبة هو لحظة حاسمة في حياة كل طالب، ومنصتنا مصممة لتسهيل هذا القرار ، و نقدم رؤى قيمة حول موقع كل جامعة، والكليات المتاحة، والمزيد..`;
+const whoISUs = ` فيما يلي بعض الاسئلة الشائعة والتي قد ترادوكم `;
 
 
 const FAQ = () => {
     return (
         <section className=" bg">
+
+
             {/* -------------Header------------- */}
-            <div className="flex lg:flex-row flex-col-reverse   justify-center items-center lg:p-32 px-10  ">
+            <div className="flex lg:flex-row flex-col-reverse   justify-center items-center lg:px-32 px-10  ">
                 <div>
-                    <h1 className="lg:text-2xl text-lg text-warning my-4 font-semibold ">إحصل على<span className=""> إستشارة </span> :</h1>
+                    <h1 className="lg:text-2xl text-lg text-warning my-4 font-semibold ">الإسئلة الشائعة :</h1>
                     <h3 className="lg:text-2xl text-sm font-medium leading-loose text-justify">{whoISUs}</h3>
                 </div>
             </div>
+
             {/* -------------Header ends------------- */}
 
 
-            <div className="  lg:px-32 p-10 my-8  ">
-                <div>
-                    <h1 className="lg:text-2xl text-lg text-warning my-4 font-semibold ">تواصل<span className="">   معنـــــــــــــا عبر  </span> :</h1>
-                    <div className="lg:text-2xl text-center font-medium leading-loose ">
-                        <p>{whoISUs}</p>
 
-                        <a href={`https://wa.me/`} target='_blank'>
-                            <button className="btn btn-wide my-8 bg-accent hover:bg-success transition-all duration-700
-                            text-white hover:text-black hover:w-72 "
-                            >اضغط للإنتقال الى المحادثة</button>
-                        </a>
+
+            <div className="   ">
+                <div className=" lg:p-28 p-10  ">
+                    <div className="collapse bg-secondary lg:my-5 my-2">
+                        <input type="checkbox" />
+                        <div className="collapse-title text-xl font-medium">
+                            هل يحيى يحب حسن ؟
+                        </div>
+                        <div className="collapse-content">
+                            <p>ممكن :)</p>
+                        </div>
+                    </div>
+                    <div className="collapse bg-secondary lg:my-5 my-2">
+                        <input type="checkbox" />
+                        <div className="collapse-title text-xl font-medium">
+                            هل يحيى يحب حسن ؟
+                        </div>
+                        <div className="collapse-content">
+                            <p>ممكن :)</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="  lg:px-32 p-10 my-8  ">
-                <div>
-                    {/* <Form></Form> */}
-                </div>
-            </div>
+
         </section>
 
 
