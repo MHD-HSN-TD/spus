fa454a89a2383cc51166052eac975a8dc6d2087b
MHD-HSN-TD <8883597!@mhd>
2024-05-13 17:05:04 +0300

    remove commented things

--- src/app/(dynamic)/unis/page.jsx
+++ src/app/(dynamic)/unis/page.jsx
@@ -1,7 +1,5 @@
-// "use client"
 import Filter from "@/components/filter/Filter"
 import ShowUnis from "@/components/showUnis/ShowUnis"
-// import { motion } from "framer-motion"
 
 
 export const metadata = {
@@ -11,10 +9,7 @@ export const metadata = {
 const Unis = () => {
     // console.log('ss', image)
     return (
-        <div className="  "
-        // initial={{ opacity: 0, x: 10 }}
-        // whileInView={{ opacity: 1, x: 0 }}
-        >
+        <div className="  ">
             {/* <div className="w-100  bg-accent-focus py-4 px-5 text-2xl"><p>الجامعات الخاصة في سوريا لعام 2023</p></div> */}
             <Filter />
             <ShowUnis></ShowUnis>
