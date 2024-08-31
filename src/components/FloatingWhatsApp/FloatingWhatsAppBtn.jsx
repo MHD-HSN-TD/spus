9e0c69e190e7e10d9e506181207f9b5fea2d2559
MHD-HSN-TD <8883597!@mhd>
2024-08-31 15:32:52 +0300

    test another animation

--- src/components/FloatingWhatsApp/FloatingWhatsAppBtn.jsx
+++ src/components/FloatingWhatsApp/FloatingWhatsAppBtn.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx';
 import { usePathname } from 'next/navigation'
+import styles from './styles.module.css'
 
 
 
@@ -16,12 +17,16 @@ const FloatingWhatsAppBtn = () => {
     return (
         <div
             //to control the className conditinaly
-            className={` ${clsx(isConsulte && ' hidden')} float
-            animate-pulse duration-[2000ms] ease-in     
-            fixed bg-warning rounded
+            className={` ${clsx(isConsulte && ' hidden')} 
+            float
+            bg-warning rounded
+            fixed 
             md:w-20  md:bottom-10 md:left-8
             bottom-6 left-3 w-16
+            animate-pulse duration-[3000ms] ease-in 
             `} >
+            {/* ${styles.bounce_out_bottom} */}
+
             <Link href="/consulting" className='text-center'>
                 <h3 className='text-black font-semibold text-sm lg:text-lg p-2'>إحصل على أستشارتك الان !</h3>
                 {/* <Image class=" m-2"
